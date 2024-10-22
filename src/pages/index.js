f0850a5ce071153ffab0769642dc11fb016abb3b
Minazuki <bintanghack32@gmail.com>
2024-10-22 09:22:00 +0700

    index.html

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+const IndexPage = () => {
+
+  /* <div class="grid grid-cols-3 gap-4">
+  <div class="...">01</div>
+  <div class="...">02</div>
+  <div class="...">03</div>
+  <div class="col-span-2 ...">04</div>
+  <div class="...">05</div>
+  <div class="...">06</div>
+  <div class="col-span-2 ...">07</div>
+</div> */
+}
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
